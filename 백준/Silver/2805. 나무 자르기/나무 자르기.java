6a64ea815dc276bb7b84c7fd3ee1afdea5d4fb48
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2022-03-18 23:07:17 +0900

    [Silver III] Title: 나무 자르기, Time: 560 ms, Memory: 119364 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,58 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.*;
+import java.util.stream.IntStream;;
 
 public class Main {
-
     static int[] arr;
-    static int m;
+    static int M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine() , " ");
+        StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
+        int N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        m = sc.nextInt();
-        arr = new int[n];
-        int max = 0;
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-            if (arr[i] > max) {
-                max = arr[i];
-            }
+        arr = new int[N];
+        int start = 0;
+        int end = 0;
+        st = new StringTokenizer(br.readLine() , " ");
+        for(int i = 0 ; i< arr.length;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        int start = 0; // 무조건 나무를 가져갈 수 있는 부분
-        int end = max; // 무조건 나무를 가져갈 수 없는 부분
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>end) end = arr[i];
+        }
 
         while (start + 1 < end) {
-            int mid = (start + end) / 2;
-
-            if (check(mid)) { // mid위치에서 나무를 가져갈 수 있는지 없는지 확인, 가져갈 수 있다면 mid는 start에 없다면 end에 넣음
+            int mid = (start+end)/2;
+            if(check(mid)){
                 start = mid;
-            } else {
-                end = mid;
-            }
+            }else end = mid;
         }
-
         System.out.println(start);
     }
 
-    static boolean check(int cuttingHeight) { // cuttingHeight위치에서 나무를 가져갈 수 있는지 없는지 확인
-
+    public static boolean check(int mid) {
         long sum = 0;
         long result;
         long tree;
-
         for (int i = 0; i < arr.length; i++) {
             tree = arr[i];
-            if (tree >= cuttingHeight) {
-                result = tree - cuttingHeight;
-                sum += result;
+            if(tree>=mid){
+                result = tree - mid;
+                sum+=result;
             }
         }
-        if (sum >= m) {
+        if (sum >= M) {
             return true;
-        } else {
-            return false;
-        }
+        }else return false;
     }
 }
+
