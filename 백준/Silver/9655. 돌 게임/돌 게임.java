e35ccb776d16b036780bb96024e239516e6b867f
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2022-04-24 02:41:41 +0900

    [Silver V] Title: 돌 게임, Time: 220 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,29 @@
+import org.w3c.dom.Node;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.IntStream;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n % 2 != 0) {
+            System.out.println("SK");
+        } else {
+            System.out.println("CY");
+        }
+    }
+}
+    // nCk
+
+
+
+
+
+
+
+
