744863f35d156946784c976ccde8a843b8d86276
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2022-10-01 21:05:10 +0900

    [Silver V] Title: 다리 놓기, Time: 152 ms, Memory: 18360 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,41 +1,42 @@
-import org.w3c.dom.Node;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+public class Main {
+    static int N = 0;
+    static int[][] dp = new int[1001][1001];
 
-class Main{
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        while (n-- > 0){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println(nCk(b, a));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            String[] input = br.readLine().split(" ");
+            // 서쪽 게이트 수 input[0]
+            // 동쪽 게이트 수 input[1]
+            int east = Integer.parseInt(input[0]);
+            int west = Integer.parseInt(input[1]);
+            int max = Math.max(east, west);
+            int min = Math.min(east, west);
+            System.out.println(dpBridge(max,min));
         }
+
     }
-    // nCk
-    public static long nCk(int n, int k){
-        long result = 1;
-        for(int i = 1; i <= k; i++){
-            result *= n - k + i;
-            result /= i;
+    // number of cases
+    // nCr = n-1Cr-1 + n-1Cr
+    public static int dpBridge(int max , int min) {
+        if(max < 0 || min < 0) return 0;
+        if(max == min) {
+            dp[max][min] = 1;
+            return 1;
         }
-        return result;
+        if(dp[max][min] != 0) {
+            return dp[max][min];
+        }
+        dp[max][min] = dpBridge(max-1,min-1) + dpBridge(max-1,min);
+        return dp[max][min];
+
     }
-
-
-
-
-
-
-
-
-
-
-
-
 }
