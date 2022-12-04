7aed0d2fd748208949620d3e7f56900df40c37bd
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2022-12-04 23:05:50 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 3.63 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,14 @@
+
+import java.util.Set;
+import java.util.TreeSet;
+class Solution {
+    public int[] solution(int[] numbers) {
+        Set<Integer> result = new TreeSet<>();
+        for (int i = 0 ; i < numbers.length; i++){
+            for (int j = i + 1; j < numbers.length; j++){
+                result.add(numbers[i] + numbers[j]);
+            }
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
