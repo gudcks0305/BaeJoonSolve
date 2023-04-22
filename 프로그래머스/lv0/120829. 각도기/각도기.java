a989e5ce360bd8a6d6101bc5ea638aacf3c53e0b
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2023-04-22 15:03:19 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        if(angle == 180) return 4;
+        else if(angle > 90) return 3;
+        else if(angle == 90) return 2;
+        else return 1;
+    }
+}
