9cf6b5fd155dbd6cb30e42a435f7041c0ef09db8
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2023-04-24 21:08:37 +0900

    [level 0] Title: 배열의 평균값, Time: 2.00 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        return Arrays.stream(numbers).average().getAsDouble();
+    }
+}
